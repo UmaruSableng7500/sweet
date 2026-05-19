cf52b1e08da57b2f102503028f49e72359ca7094
UmaruSableng7500 <umarusableng@proton.me>
2026-05-19 02:11:10 -0700

    sweet: implement vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+# miui camera (sweet)
+git clone https://github.com/PixelOS-Devices/android_device_xiaomi_miuicamera-sweet.git device/xiaomi/miuicamera-sweet
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_miuicamera-sweet.git vendor/xiaomi/miuicamera-sweet
+
+# commons
+git clone https://github.com/PixelOS-Devices/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+# sweet vendor
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+# dolby
+git clone https://github.com/sweet-stuffs/proprietary_vendor_sony_dolby.git vendor/sony/dolby
+
+# kernel sm6150
+git clone https://github.com/PixelOS-Devices/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+
+# hardware xiaomi
+git clone https://github.com/RiteshSahany/hardware_xiaomi.git hardware/xiaomi
